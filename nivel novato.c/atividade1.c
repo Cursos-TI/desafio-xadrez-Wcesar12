14eae0eebd8121144c3a14dbbae09aa2f1ac0c52
Wcesar12 <202501587781@alunos.unifanor.edu.br>
2025-05-27 23:06:17 +0000

    movimento das pecas

--- nivel novato.c/atividade1.c
+++ nivel novato.c/atividade1.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    //***Movimento da Torre*** 
-    // Mover 5 casas para a Direita
+    // Movimento da Torre
     int Torre = 5;
     printf("Movimento da Torre (5 casas para a Direita):\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); 
-        //Imprime a direcao do movimento
+    for (int i = 0; i < Torre; i++) {
+        printf("Direita\n");
     }
 
     printf("\n");
 
-    // ***Movimento do Bispo***
-    // Mover 5 casas para Cima Direita.
+    // Movimento do Bispo
     int Bispo = 5;
-    int i = 0;
+    int i = 0; 
     printf("Movimento do Bispo (5 casas na Diagonal Cima Direita):\n");
-    while (i <= Bispo) {
+    while (i < Bispo) {
         printf("Cima Direita\n");
         i++;
-        //Iprime a direçao do movimento
     }
 
     printf("\n");
 
-    //***Movimento da Rainha***
-    // Se Move em todas as direções. Vamos mover 8 casas para a Esquerda.
+    // Movimento da Rainha
     int Rainha = 8;
-    int i = 0;
+    i = 0;
     printf("Movimento da Rainha (8 casas para a Esquerda):\n");
     do {
         printf("Esquerda\n");
         i++;
-    } while (i <= Rainha);
+    } while (i < Rainha);
 
     return 0;
 }
